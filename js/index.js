cfc66dc39113f1d0c1987d5b2d0fa191e9fbbd70
wangsir <1433256766@qq.com>
2018-05-07 14:57:09 +0800

    修改悬浮效果

--- js/index.js
+++ js/index.js
@@ -30,9 +30,21 @@ $(function(){
     });
 
     //悬浮客服，鼠标悬浮
-    $(".fix_kefu .fix_kefu_nav .kefu_tel a").hover(function(){
-    	$(".fix_kefu  .kefu_none .kefu_tel").css('display', 'block');
+    $(".fix_kefu .fix_kefu_nav .kefu_tel a,.fix_kefu  .kefu_none .kefu_tel").hover(function(){
+    	$(".fix_kefu  .kefu_none .kefu_tel").toggle();
+    });
+    $(".fix_kefu .fix_kefu_nav .kefu_tel a,.fix_kefu  .kefu_none .kefu_tel").hover(function(){
+    	$(".fix_kefu .fix_kefu_nav .kefu_tel a").css('border-left', 0);
     },function(){
-    	$(".fix_kefu  .kefu_none .kefu_tel").css('display', 'none');
+    	$(".fix_kefu .fix_kefu_nav .kefu_tel a").css('border-left', '1px solid #387a21');
+    })
+
+    $(".fix_kefu .fix_kefu_nav .kefu_bindweixin a,.fix_kefu .kefu_none .kefu_weixin").hover(function(){
+    	$(".fix_kefu .kefu_none .kefu_weixin").toggle();
+    });
+    $(".fix_kefu .fix_kefu_nav .kefu_bindweixin a,.fix_kefu  .kefu_none .kefu_weixin").hover(function(){
+    	$(".fix_kefu .fix_kefu_nav .kefu_bindweixin a").css('border-left', 0);
+    },function(){
+    	$(".fix_kefu .fix_kefu_nav .kefu_bindweixin a").css('border-left', '1px solid #387a21');
     })
 })
